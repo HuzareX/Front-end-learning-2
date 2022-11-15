ce7855584e16073fe0090f1d42a5ad3b9d1330ae
HuzareX <danielwegierski@gmail.com>
2022-11-15 22:03:07 +0100

    Added section 'achievments' using FontAwesome and Bootstrap styles.

--- js/slicksettings.js
+++ js/slicksettings.js
@@ -2,7 +2,25 @@ $('.team-carousel').slick({
     autoplay: true,
 	arrows: false,
     mobileFirst: true,
-	infinite: true,
-	slidesToShow: 2,
-	slidesToScroll: 2,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	responsive: [
+		{
+		  breakpoint: 768,
+		  settings: {
+			slidesToShow: 2,
+			slidesToScroll: 2,
+			infinite: true,
+			dots: true
+		  }
+		},
+		{
+		  breakpoint: 992,
+		  settings: {
+			slidesToShow: 3,
+			slidesToScroll: 3,
+		  }
+		},
+
+	]
 })
